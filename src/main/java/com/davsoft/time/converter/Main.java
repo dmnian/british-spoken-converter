e4c62fd8cc9cc9cc1de33b8769d1eb98948b5fe5
Damian D <dmnian@gmail.com>
2023-08-16 11:15:04 +0200

    add java project with maven

---
+++ src/main/java/com/davsoft/time/converter/Main.java
@@ -0,0 +1,7 @@
+package com.davsoft.time.converter;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Welcome in the British spoken time converter!\n");
+    }
+}
